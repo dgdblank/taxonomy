58e49bcb8e5f07f0478c7d756bdc044a80032127
Hack Reactor Students <accounts@hackreactor.com>
2015-05-20 15:04:57 -0700

    complete RetiredForager requirements

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,17 @@
-var RetiredForagerBee = function() {
+var RetiredForagerBee = function(age, color, food, job, canFly, treasureChest) {
+  ForagerBee.call(this, age, color, food, job, canFly, treasureChest);
 };
+
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function(){
+  return 'I am too old, let me play cards instead';
+};
+
+RetiredForagerBee.prototype.gamble = function(){
+  this.treasureChest.push('treasure');
+};
+
+
 
--- test/RetiredForagerBee.js
+++ test/RetiredForagerBee.js
@@ -3,7 +3,7 @@ describe('RetiredForagerBee class functionality', function() {
   verifyClass(RetiredForagerBee).followsPattern('pseudoclassical', {}, false);
 
   beforeEach(function() {
-    retiredForager = new RetiredForagerBee();
+    retiredForager = new RetiredForagerBee(40, 'grey', 'jelly', 'gamble', false, []);
   });
 
   /*  Overwrite methods from superclass  */
