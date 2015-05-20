d69fceb438056b290eb305c3db21ea9c1515580b
Hack Reactor Students <accounts@hackreactor.com>
2015-05-20 14:42:54 -0700

    Completed HoneyMakerBee Requirements

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(age, color, food, job, honeyPot) {
+  Bee.call(this, age, color, food, job);
+  this.honeyPot = honeyPot;
 };
 
+//eat
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+//
+HoneyMakerBee.prototype.makeHoney = function(){
+  this.honeyPot++;
+};
+//
+HoneyMakerBee.prototype.giveHoney = function(){
+  this.honeyPot--;
+};
