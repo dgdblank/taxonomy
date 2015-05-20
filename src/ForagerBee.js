e36761479821193f22d7febcc83f3a12c44d78e0
Hack Reactor Students <accounts@hackreactor.com>
2015-05-20 14:51:30 -0700

    complete ForagerBee requirements

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-var ForagerBee = function() {
+var ForagerBee = function(age, color, food, job, canFly, treasureChest) {
+  Bee.call(this, age, color, food, job);
+  this.canFly = canFly;
+  this.treasureChest = treasureChest;
+};
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(treasure){
+  this.treasureChest.push(treasure);
 };
 
